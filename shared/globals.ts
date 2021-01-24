0b7cdcded09f5bd38586bfe041a606f7eb38b81b
Lance Armah-Abraham <lanarmah@gmail.com>
2021-01-24 22:42:21 +0000

    added styles

--- shared/globals.ts
+++ shared/globals.ts
@@ -2,8 +2,12 @@ import {css} from '@emotion/react'
 
 const globalStyles = css`
   :root {
-    --page-background: #222;
-    --page-text: #ddd;
+    --page-background: rgb(248, 248, 248);
+    --page-text: rgb(40,40,40);
+    --background-accent: rgb(3,84,133);
+    --color-text-invert: rgb(240,240,240);
+    --color-accent: rgb(4,64,100);
+    --page-width: 800px;
   }
 
   *,
@@ -18,6 +22,11 @@ const globalStyles = css`
     fontFamily: "-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif"
     background-color: var(--page-background)
     color: var(--page-text)
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
   }
 `
 
