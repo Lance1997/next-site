244c1e63fe8cdbf66fa3d965ff29eea32bdf04a2
Lance Armah-Abraham <lanarmah@gmail.com>
2021-01-25 08:39:38 +0000

    added logic to display posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,41 @@
+import {InferGetStaticPropsType} from 'next'
+import { getPosts } from '@shared/get-posts'
+import Link from 'next/link'
+
 import Page from '@layouts/page';
 
-export default function Home() {
+export default function Home({posts}: InferGetStaticPropsType <typeof getStaticProps>) {
   return (
-    <Page  />
+    <Page>
+      <ul>
+        {
+          posts.map(post => {
+           return (
+              <li key={post.slug}>
+                <Link href={`/post/${post.slug}`}>
+                  <a>
+                  {post.frontMatter.title}
+                  </a>
+                </Link>
+              </li>
+           )
+          })
+        }
+      </ul>
+    </Page>
   )
+}
+
+export const getStaticProps = async () => {
+  const posts = await getPosts('./posts')
+  const allMdx = posts.map(({slug, frontMatter}) => {
+    return {
+      slug, frontMatter
+    }
+  })
+  return {
+    props: {
+      posts: allMdx
+    }
+  }
 }
