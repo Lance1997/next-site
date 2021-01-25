12181358c2e5d97c09e78793f572960b2e8b5081
Lance Armah-Abraham <lanarmah@gmail.com>
2021-01-25 07:04:18 +0000

    extracted seo as component

--- layouts/page.tsx
+++ layouts/page.tsx
@@ -1,24 +1,25 @@
 import {ReactNode} from 'react'
-import Head from 'next/head'
 import Header from '@components/header'
 import Navigation from '@components/navigation'
+import Footer from '@components/footer'
+import SEO, {SEOProps} from '@components/seo'
 
 type PageProps = {
+  meta?: SEOProps
   children?: ReactNode
 }
 
-export default function Page({children}: PageProps) {
+export default function Page({meta, children}: PageProps) {
   return (
     <>
-      <Head>
-        <title>NextJS Tuts</title>
-      </Head>
+      <SEO {...meta} />
       
-        <main>
-          <Header />
-          <Navigation />
-          {children}
-        </main>
+      <main>
+        <Header />
+        <Navigation />
+        {children}
+      </main>
+      <Footer />
     </>
   )
 }
