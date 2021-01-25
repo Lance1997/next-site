32d9dc519487431580395e778d87b062de2cdc9c
Lance Armah-Abraham <lanarmah@gmail.com>
2021-01-25 08:40:04 +0000

    added logic to support types for next-mdx-remote

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,3 +1,31 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
 /// <reference types="@emotion/react/types/css-prop" />
+
+type RenderToStringParams = {
+    components?: object
+    mdxOptions?: object
+    scope?: object
+}
+
+type HydrateParams = {
+    components?: object
+}
+
+declare module 'next-mdx-remote/render-to-string' {
+    function renderToString(
+        source: string,
+        RenderToStringParams: RenderToStringParams
+    ): object
+
+    export default renderToString
+}
+
+declare module 'next-mdx-remote/hydrate' {
+    function hydrate(
+        mdxObject: object,
+        components?: HydrateParams
+    ): string
+
+    export default hydrate
+}
