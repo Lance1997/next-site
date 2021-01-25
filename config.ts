aec46266433709acadd6d9b22f781830f1ba318d
Lance Armah-Abraham <lanarmah@gmail.com>
2021-01-25 07:03:25 +0000

    added app config for general settings

---
+++ config.ts
@@ -0,0 +1,12 @@
+const metadata = {
+    siteName: "Statr", 
+    description: "Statr NextJS Tutorials Website",
+    author: {
+        twitter: "@ArmahLance",
+        twitterUrl: "https://twitter.com/ArmahLance",
+        githubUrl: "https://github.com/lance1997",
+        linkedinUrl: "https://linkedin.com/in/lance-armah-abraham"
+    }
+}
+
+export {metadata}
